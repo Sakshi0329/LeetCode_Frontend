813a875f6b8aefa38b199fd900ddbcf66f75abec
Sakshi0329 <sakshiparab0302@gmail.com>
2026-01-04 00:01:47 +0530

    update axiosClient

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -3,10 +3,22 @@ import axios from "axios";
 
 const axiosClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true,
+  withCredentials: false,
   headers: {
     "Content-Type": "application/json",
   },
 });
+axiosClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default axiosClient;
